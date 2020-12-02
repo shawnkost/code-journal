2835b9754fb4a904c3ac542b85f425e571ca6613
Shawn Kost <shawnmkost@gmail.com>
2020-12-02 11:03:27 -0800

    Stored the users data in local storage so it persists between reloads. #1

--- js/main.js
+++ js/main.js
@@ -9,10 +9,20 @@ $avatar.addEventListener('input', function (event) {
 
 var $form = document.querySelector('form');
 
-$form.addEventListener('submit', function (event) {
+$form.addEventListener('submit', function formEvent (event) {
   data.profile.avatarUrl = event.target[0].value;
   data.profile.username = event.target[1].value;
   data.profile.fullName = event.target[2].value;
   data.profile.location = event.target[3].value;
   data.profile.bio = event.target[4].value;
 });
+
+var userData = localStorage.getItem("code-journal");
+if (userData !== null) {
+  data = JSON.parse(userData);
+}
+
+window.addEventListener("beforeunload", function (event) {
+  dataJSON = JSON.stringify(data);
+  localStorage.setItem("code-journal", dataJSON);
+})
