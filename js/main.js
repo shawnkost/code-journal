47999a22a74855a4b55b822338786cfa348af4e1
Shawn Kost <shawnmkost@gmail.com>
2020-12-01 20:53:22 -0800

    Assigned all form values to the profile data model. #1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 var $avatar = document.querySelector('#avatar-url');
 
 var $img = document.querySelector('.img');
@@ -9,5 +10,10 @@ $avatar.addEventListener('input', function (event) {
 var $form = document.querySelector('form');
 
 $form.addEventListener('submit', function (event) {
-
+  event.preventDefault();
+  data.profile.avatarUrl = event.target[0].value;
+  data.profile.username = event.target[1].value;
+  data.profile.fullName = event.target[2].value;
+  data.profile.location = event.target[3].value;
+  data.profile.bio = event.target[4].value;
 });
