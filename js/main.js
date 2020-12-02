152d73d86aeeba894084e841d99e465524148495
Shawn Kost <shawnmkost@gmail.com>
2020-12-02 11:25:30 -0800

    Removed all CSS that was unnecessary. #1

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $avatar.addEventListener('input', function (event) {
 
 var $form = document.querySelector('form');
 
-$form.addEventListener('submit', function formEvent (event) {
+$form.addEventListener('submit', function formEvent(event) {
   data.profile.avatarUrl = event.target[0].value;
   data.profile.username = event.target[1].value;
   data.profile.fullName = event.target[2].value;
@@ -17,12 +17,12 @@ $form.addEventListener('submit', function formEvent (event) {
   data.profile.bio = event.target[4].value;
 });
 
-var userData = localStorage.getItem("code-journal");
+var userData = localStorage.getItem('code-journal');
 if (userData !== null) {
   data = JSON.parse(userData);
 }
 
-window.addEventListener("beforeunload", function (event) {
-  dataJSON = JSON.stringify(data);
-  localStorage.setItem("code-journal", dataJSON);
-})
+window.addEventListener('beforeunload', function (event) {
+  var dataJ = JSON.stringify(data);
+  localStorage.setItem('code-journal', dataJ);
+});
