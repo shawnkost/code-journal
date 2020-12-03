416dd8d5676b16db2fcdc03bd64fc6116cb5a207
Shawn Kost <shawnmkost@gmail.com>
2020-12-02 17:17:40 -0800

    Defined a function that takes the profile data model and returns a DOM tree along with inputting the values from the profile data. #2

--- js/main.js
+++ js/main.js
@@ -26,3 +26,68 @@ window.addEventListener('beforeunload', function (event) {
   var dataJ = JSON.stringify(data);
   localStorage.setItem('code-journal', dataJ);
 });
+
+function domTree(profile) {
+  var div1 = document.createElement('div');
+  div1.setAttribute('data-view', 'profile');
+
+  var div2 = document.querySelector('.edit-h1');
+  div1.appendChild(div2);
+
+  var h1 = document.querySelector('.fname-text');
+  h1.textContent = data.profile.fullName;
+  div2.appendChild(h1);
+
+  var div3 = document.createElement('div');
+  div3.setAttribute('class', 'row');
+  div1.appendChild(div3);
+
+  var div4 = document.createElement('div');
+  div4.setAttribute('class', 'column-half');
+  div3.appendChild(div4);
+
+  var userPhoto = document.querySelector('.img2');
+  userPhoto.setAttribute('src', data.profile.avatarUrl);
+  div4.appendChild(userPhoto);
+
+  var div5 = document.createElement('div');
+  div5.setAttribute('class', 'column-half profile-details');
+  div3.appendChild(div5);
+
+  var div6 = document.createElement('div');
+  div6.setAttribute('class', 'container');
+  div5.appendChild(div6);
+
+  var icon1 = document.querySelector('.icon1');
+  div6.appendChild(icon1);
+
+  var div7 = document.querySelector('.username-text');
+  div7.textContent = data.profile.username;
+  div6.appendChild(div7);
+
+  var div8 = document.createElement('div');
+  div8.setAttribute('class', 'container');
+  div5.appendChild(div8);
+
+  var icon2 = document.querySelector('.icon2');
+  div8.appendChild(icon2);
+
+  var div9 = document.querySelector('.location-text');
+  div9.textContent = data.profile.location;
+  div8.appendChild(div9);
+
+  var div10 = document.createElement('div');
+  div10.setAttribute('class', 'container');
+  div5.appendChild(div10);
+
+  var p1 = document.querySelector('.bio-text1');
+  p1.textContent = data.profile.bio;
+  div10.appendChild(p1);
+
+  var p2 = document.querySelector('.bio-text2');
+  p2.textContent = data.profile.bio;
+  div10.appendChild(p2);
+
+  return div1;
+}
+domTree(data.profile);
